1977db3c99bfbcdf2f98f99b4c137e9cb750ca56
chisam0217 <60233725+chisam0217@users.noreply.github.com>
2020-07-12 15:10:53 -0400

    Update generate_perturbation.py

--- GUA/generate_perturbation.py
+++ GUA/generate_perturbation.py
@@ -67,7 +67,7 @@ adj = torch.from_numpy(adj.astype(np.float32))
 # print (idx_train.shape)
 # print (idx_val.shape)
 # print (idx_test)
-# s = adj.shape[0]
+
 # Model and optimizer
 model = GCN(nfeat=features.shape[1],
             nhid=args.hidden,
@@ -129,8 +129,6 @@ for epoch in range(args.epochs):
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
-# torch.save(model, './cora_gcn.pth')
-# torch.save(model.state_dict(), 'cora_gcn.pkl')
 
 # Testing
 ori_output = test(adj)
@@ -139,35 +137,26 @@ def calculate_grad(pert_adj, idx, classes):
     x = Variable(pert_adj, requires_grad=True)
     output = model(features, x)
     grad = []
-#     for i in range(classes):
+
     for i in classes:
         cls = torch.LongTensor(np.array(i).reshape(1)).cuda()
-        #adtop L1 loss
-#         loss = F.nll_loss(output[idx:idx+1], cls)
         loss = F.nll_loss(output[idx:idx+1], cls) 
-#     + args.L2 * torch.norm(pert_adj[idx], p=2) 
-#     args.L1 * torch.norm(pert_adj[idx], p=1)
-#         print ('the loss is', loss)
         loss.backward(retain_graph=True)
         grad.append(x.grad[idx].cpu().numpy())
-#     print ('grad', grad)
+
     return np.array(grad)   
 
 
 def add_perturb(input_adj, idx, perturb):
     # (1-x)A + x(1-A)
-#     input_adj = input_adj.toarray()
     x = np.zeros((input_adj.shape[0], input_adj.shape[1]))
     x[idx] = perturb  
     x[:,idx] = perturb
-#     print ('x', x[idx])
 
     
-#     x += np.transpose(x) #change the idx'th row and column
+ #change the idx'th row and column
     x1 = np.ones((input_adj.shape[0], input_adj.shape[1])) - x
-#     print ('x1', x1[idx])
     adj2 = np.ones((input_adj.shape[0], input_adj.shape[1])) - input_adj
-#     print ('adj2', adj2[idx])
 
     for i in range(input_adj.shape[0]):   
         adj2[i][i] = 0
@@ -181,11 +170,9 @@ def proj_lp(v, xi=args.radius, p=2):
     # Project on the lp ball centered at 0 and of radius xi
 
     # SUPPORTS only p = 2 and p = Inf for now
-#     print ('the distance of v', np.linalg.norm(v.flatten(1)))
     
     if p == 2:
         v = v * min(1, xi/np.linalg.norm(v.flatten(1)))
-        # v = v / np.linalg.norm(v.flatten(1)) * xi
     elif p == np.inf:
         v = np.sign(v) * np.minimum(abs(v), xi)
     else:
@@ -196,7 +183,6 @@ def proj_lp(v, xi=args.radius, p=2):
     #to reduce the number of nonzero elements which means 
     #the times of perturbation, also prevents saddle point
 
-#     v = np.where(v>0.5, 1, 0)
     return v
 
 def select_pert(pert_m):
@@ -234,63 +220,40 @@ def deepfool(innormal_adj, ori_adj, idx, num_classes, degree, overshoot=0.02, ma
     
     I = pred.argsort()[::-1]
     I = I[0:num_classes]
-    label = I[0]
-    
+    label = I[0]    
     f_i = np.array(pred).flatten()
-
     k_i = int(np.argmax(f_i))
-    
     w = np.zeros(ori_adj.shape[0])
     r_tot = np.zeros(ori_adj.size(0))
     
-#     pert_adj = ori_adj
     pert_adj = ori_adj.detach().cpu().numpy()
     pert_adj_tensor = ori_adj
     degree_idx = degree
     loop_i = 0
     while k_i == label and loop_i < max_iter:
         pert = np.inf
-#         gradients = calculate_grad(pert_adj_tensor, idx, num_classes)
         gradients = calculate_grad(pert_adj_tensor, idx, I)
         for i in range(1, num_classes):
             # set new w_k and new f_k
             w_k = gradients[i, :] - gradients[0, :]
             f_k = f_i[I[i]] - f_i[I[0]]
             pert_k = abs(f_k)/np.linalg.norm(w_k.flatten())
-#             print ('num_classes', num_classes)
-#             print ('pert_k', pert_k)
-#             print ('w_k', w_k)
-#             print ('np.linalg.norm(w_k.flatten())', np.linalg.norm(w_k.flatten()))
-#             print ('abs(f_k)', abs(f_k))
+
             # determine which w_k to use
             if pert_k < pert:
                 pert = pert_k
                 w = w_k
-        #this is for the polblogs
-#         if sum(w) == 0:
-#             break
-        # compute r_i and r_tot 
+
         r_i =  pert * w / np.linalg.norm(w)
-#         r_i = convert_to_v(innormal_adj, r_i, idx) #x_change = A'_change / (1-2A)
+
         r_tot = r_tot + r_i
         
-        # compute new perturbed image
-#         r_tot = np.random.rand(ori_adj.size(0))
-#         r_tot = np.random.randint(10, size=ori_adj.size(0))
-#         r_tot[0] = 2
-#         r_tot[1] = 3
-#         r_tot[1:] = 0
-
 
         pert_adj, _ = normalize_add_perturb(pert_adj, idx, (1+overshoot)*r_tot, degree_idx)
-#         print ('pert_adj in loop', pert_adj[idx])
-#         pert_adj = add_perturb(innormal_adj, idx, np.multiply((1+overshoot)*r_tot, degree[idx]))
-#         pert_adj = np.where(pert_adj<0.5, 0, 1)
         #################
         pert_adj = np.clip(pert_adj, 0, 1)
-
         #################
-#         pert_adj, _ = normalize(pert_adj + np.eye(ori_adj.shape[0]))
+
         loop_i += 1
         
         # compute new label
@@ -298,12 +261,6 @@ def deepfool(innormal_adj, ori_adj, idx, num_classes, degree, overshoot=0.02, ma
         pert_adj_tensor = pert_adj_tensor.cuda()
         f_i = np.array(model(features, pert_adj_tensor)[idx].detach().cpu().numpy()).flatten()
         k_i = int(np.argmax(f_i))
-#         print ('degree', degree[idx])
-#         print ('original conn', ori_adj[idx])
-#         print ('r_tot', r_tot)
-#         print ('perturbed conn row', pert_adj[idx])
-#         print ('output', f_i)
-#         print ('the predict class', k_i)
         if k_i != label:
             print ('attack succeed')
     
@@ -322,8 +279,6 @@ def universal_attack(attack_epoch, max_epoch):
     max_iter_df = 30
 
     v = np.zeros(tmp_adj.shape[0]).astype(np.float32)
-    # stdv = 1./math.sqrt(tmp_adj.shape[0])
-    # v = np.random.uniform(-stdv, stdv, tmp_adj.shape[0])
 
     cur_foolingrate = 0.0
     epoch = 0
@@ -346,7 +301,6 @@ def universal_attack(attack_epoch, max_epoch):
             print ('deepfool node',k)
             #add v to see if the attack succeeds
             innormal_x_p = add_perturb(tmp_adj, k, v)
-
             ##################whether to use filtering
     #         innormal_x_p = np.where(innormal_x_p<0.5, 0, 1)
 
@@ -359,31 +313,17 @@ def universal_attack(attack_epoch, max_epoch):
 
             if int(torch.argmax(output[k])) == int(torch.argmax(ori_output[k])):
                 dr, iter = deepfool(innormal_x_p, x_p, k, num_classes, degree_p[k], overshoot, max_iter_df )
-
-
-    #             print ('dr', dr)
-    #             print ('iter', iter)
-                # print ('the old perturbation matrix wass', v)
-                # print ('the old distance of perturbation is', np.linalg.norm(v.flatten(1)))
                 if iter < max_iter_df-1:
                     v = v + dr
 
                     # Project on l_p ball
                     v = proj_lp(v)
-    #                 print ('L1 norm ov v', torch.norm(v, p=1))
-    #                 print ('L2 norm ov v', torch.norm(v, p=2))
+
                 else:
                     print ('cant attack this node')
             else:
                 print ('attack succeed')
-
-    #         print ('the prediction of k node', int(torch.argmax(output[k])))
-    #         print ('the true label', int(labels[k]))
         print ('the deepfooling time cost is', time.time()-attack_time)
-
-        ###################################################
-    #     v = np.random.rand(tmp_adj.shape[0])
-
 
         print ('the perturbation matrix is', v)
         print ('testing the attack success rate')
@@ -408,7 +348,6 @@ def universal_attack(attack_epoch, max_epoch):
                 res.append(1)
         fooling_rate = float(sum(res)/len(res))
         print ('the current fooling rate is', fooling_rate)
-        # print ('the current fooling rate is', fooling_rate)
 
         ####################
         # if fooling_rate > cur_foolingrate:
